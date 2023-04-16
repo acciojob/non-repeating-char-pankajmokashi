2043f0c332036990fb5a5749bfd61ad87750250e
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-04-16 21:56:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,19 @@ let str = prompt("Enter a string");
 let arr = str.split("")
 
 arr.sort()
-for(let i = 0; i < arr.length - 1; i++)
+for(let i = 0; i < arr.length; i++)
 {
     if(arr[i] != arr[i + 1])
     {
         alert(arr[i])
+        break
+    }
+    else{
+        let j = i + 1
+        while(arr[i] == arr[j] && j < arr.length)
+        {
+            j++;
+        }
+        i = j - 1
     }
 }
